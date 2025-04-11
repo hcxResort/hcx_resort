720a504b696ba14f8d98814cb51a7653276e7cef
daniel yilma <test@gmail.com>
2025-04-11 10:31:34 +0300

    configuring the static files

--- hcx_resort/urls.py
+++ hcx_resort/urls.py
@@ -5,6 +5,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from core.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Swagger setup
 schema_view = get_schema_view(
@@ -42,3 +44,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("swagger.yml", schema_view.without_ui(cache_timeout=0), name="schema-yml"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
