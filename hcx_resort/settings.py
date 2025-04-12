e9f3641ea90500f6e2871e1ace64d25b0c01c9a7
Danielyilma <105011761+Danielyilma@users.noreply.github.com>
2025-04-12 13:41:08 +0300

    Update settings.py

--- hcx_resort/settings.py
+++ hcx_resort/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'hcx_resort.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
