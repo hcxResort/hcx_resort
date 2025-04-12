8eb140fd341d4324f525c081f10a3e93d3261310
Danielyilma <105011761+Danielyilma@users.noreply.github.com>
2025-04-12 13:56:43 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    UserProfile, RoomType, Room, ServiceType, Service,
+    Reservation, ReservationService, Payment, Review
+)
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(RoomType)
+admin.site.register(Room)
+admin.site.register(ServiceType)
+admin.site.register(Service)
+admin.site.register(Reservation)
+admin.site.register(ReservationService)
+admin.site.register(Payment)
+admin.site.register(Review)
